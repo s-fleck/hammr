65e4e9ebd90391f153cfdbc022d4d9fb377729a7
FLECK Stefan <FLECK$@statistik.local>
2016-08-16 17:41:56 +0200

    updated assertions

--- tests/testthat/test_assertions.R
+++ tests/testthat/test_assertions.R
@@ -4,7 +4,8 @@ testdat <- quarter('2015-Q1')
 
 
 test_that("mass typecasting data.frame columns works.", {
-  expect_silent('blah' %is_class% 'character')
+  expect_warning('blah' %is_class% 'character')
+  expect_silent('blah' %assert_class% 'character')
 })
 
 
