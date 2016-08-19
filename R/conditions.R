217ea9d72093ff3c2f72de0138a86bac350e1ed2
FLECK Stefan <FLECK$@statistik.local>
2016-08-19 08:51:25 +0200

    Added constructor for conditions (based on Advanced R)

---
+++ R/conditions.R
@@ -0,0 +1,44 @@
+#' Condition constructor
+#'
+#' From http://adv-r.had.co.nz/beyond-exception-handling.html
+#'
+#' @param subclass
+#' @param message
+#' @param call
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' # Construct a custom condition
+#'    malformed_log_entry_error <- function(text) {
+#'      msg <- paste0("Malformed log entry: ", text)
+#'      condition(c("malformed_log_entry_entry", "error"),
+#'            message = msg,
+#'            text = text
+#'            )
+#'    }
+#'
+#'
+#' # Signal the condition
+#'    parse_log_entry <- function(text) {
+#'      if (!well_formed_log_entry(text)) {
+#'      stop(malformed_log_entry_error(text))
+#'    }
+#'
+#'
+#' # Handle the condition
+#' tryCatch(
+#'   malformed_log_entry = function(e) NULL,
+#'   parse_log_entry(text)
+#'   )
+#'
+#'
+condition <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = c(subclass, "condition"),
+    list(message = message, call = call, ...)
+  )
+}
