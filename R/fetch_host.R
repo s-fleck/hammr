c694187d045054e6d5ef3dca575df96df062acea
FLECK Stefan <FLECK$@statistik.local>
2016-08-23 14:07:03 +0200

    fetch_host now accepts list of files

--- R/fetch_host.R
+++ R/fetch_host.R
@@ -24,9 +24,9 @@ fetch_ftp <- function(.file, .outdir = '.', .creds = NULL, .overwrite = FALSE) {
   # Setup paths and credentials ----
     outfile                     <- file.path(.outdir, .file)
 
-    if (file.exists(outfile)) {
+    if (any(file.exists(outfile))) {
       if(.overwrite){
-        warning('Zieldatei existiert und wird ueberschrieben')
+        warning('Mindestens eine Zieldatei existiert und wird ueberschrieben')
       } else {
         stop('Breche ab: Zieldatei existiert und overwrite = FALSE')
       }
@@ -42,13 +42,22 @@ fetch_ftp <- function(.file, .outdir = '.', .creds = NULL, .overwrite = FALSE) {
       file.remove(ftp_commands)
     }
 
+  # Create ftp command file
+    logon  <- paste0("user ", .creds$user, "\n", .creds$pw, "\n", "cd .. \n")
+    gets   <- paste('get', .file, collapse = '\n')
+    logoff <- "\nquit"
+    coms <- paste0(logon, gets, logoff)
+    writeLines(coms, ftp_commands)
+
 
   # Fetch file via ftp
-  writeLines(paste0("user ", .creds$user, "\n", .creds$pw, "\n","cd ..","\n","get ", .file, "\n","quit"), ftp_commands)
   cmd    <- paste0("ftp -n -s:", ftp_commands," mfstat01")
   ftplog <- shell(cmd, intern = TRUE)
 
-  message(paste(ftplog, collapse = '\n'))
+  message('FTP Log\n',
+          '---------------------------------------------------------------------\n',
+           paste(ftplog, collapse = '\n'), '\n',
+          '---------------------------------------------------------------------\n')
   file.remove(ftp_commands)
 
 
@@ -72,18 +81,21 @@ fetch_ftp <- function(.file, .outdir = '.', .creds = NULL, .overwrite = FALSE) {
   # Move file to destination dir
   copy_ok <- file.copy(from = .file, to = outfile, overwrite = .overwrite)
 
+  message('Successfully transferred ',   sum(copy_ok), ' of ', length(.file), ' files.\n')
 
-  if(copy_ok) {
-    file.remove(file)
-    cat("File saved to: ", outfile)
+  if(all(copy_ok)) {
+    file.remove(.file)
+    message("File(s) saved to: ", .outdir, '\n')
     invisible(TRUE)
+  } else if (sum(copy_ok > 0)){
+    file.remove(.file)
+    message("File(s) saved to: ", .outdir, '\n')
+    stop('Not all files were transferred successfully')
   } else {
-    stop('Something went wrong')
+    stop('Something went wrong. No files were transferred successfully')
   }
 
 }
-
-
 
 
 
