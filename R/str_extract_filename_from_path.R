51b0ebc65afeb03f729a57b0d56476d7a5553915
stefan <unabledispute@gmail.com>
2016-11-05 22:30:53 +0100

    refactoring, added function to get file extension

--- R/str_extract_filename_from_path.R
+++ R/str_extract_filename_from_path.R
@@ -12,8 +12,13 @@
 #'
 #'  extract_filename_from_path(x)
 #'  extract_filename_from_path(x, ext = FALSE)
-extract_filename_from_path <- function(x, ext = TRUE){
-  res <- strsplit(x, '/') %>%
+extract_filename_from_path <- function(x, fsep = NULL, ext = TRUE){
+
+  if(is.null(fsep)){
+    fsep <- guess_fsep(x)
+  }
+
+  res <- strsplit(x, fsep) %>%
     unlist() %>%
     tail(1)
 
@@ -26,4 +31,32 @@ extract_filename_from_path <- function(x, ext = TRUE){
   }
 
   return(res)
+}
+
+
+extract_extension_from_path <- function(x, fsep = NULL){
+  res <- extract_filename_from_path(x)
+
+  res %>%
+    strsplit(., '.', fixed = TRUE) %>%
+    unlist() %>%
+    extract(length(.))
+}
+
+
+# Utility Functions ----
+
+guess_fsep <- function(x){
+  if(is.null(fsep)){
+    a <- length(unlist(stringi::stri_locate_all(x, regex = '\\\\')))
+    b <- length(unlist(stringi::stri_locate_all(x, regex = '/')))
+
+    if(a > b){
+      fsep <- '\\\\'
+    } else {
+      fsep <- '/'
+    }
+  }
+
+  return(fsep)
 }
