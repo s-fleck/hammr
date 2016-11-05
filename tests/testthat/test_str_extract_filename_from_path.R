2df81c28edc85d5f8218f05368e4ea86631e5c24
stefan <unabledispute@gmail.com>
2016-11-05 22:31:21 +0100

    adding tests

--- tests/testthat/test_str_extract_filename_from_path.R
+++ tests/testthat/test_str_extract_filename_from_path.R
@@ -1,10 +1,16 @@
 context("extract_filename_from_path")
 
 test_that("extracting filename from path works", {
-  x <- c("C:/path/to/test.file.ext")
+  x <- c("/home/to/test.file.ext")
+  y <- c("C:\\path\\to\\test.file.ext")
+
 
   expect_identical(extract_filename_from_path(x), "test.file.ext")
   expect_identical(extract_filename_from_path(x, ext = FALSE), "test.file")
+
+  extract_filename_from_path(y)
+
+
 })
 
 
