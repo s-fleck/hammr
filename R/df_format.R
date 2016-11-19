028c3a47b38c6bdf2227d16c0e60ae5d817ddfc7
stefan <unabledispute@gmail.com>
2016-11-19 10:22:52 +0100

    completely redisgned function and added additional input parameters

--- R/df_format.R
+++ R/df_format.R
@@ -1,46 +1,71 @@
-#' Title
+#' Format columns of a data.frame
 #'
-#' @param dat
-#' @param num_format
-#' @param date_format
-#' @param format_fun
+#' Utility function to apply formating to each column of a data frame.
+#' Input can bei either a list of paramters to \link{\code{formatC}},
+#' or an arbitrary function to be applied to each column.
+#'
+#' @param dat a \code{data.frame}
+#' @param num_formatC a list of parameters to formatC
+#' @param date_formatC a list of parameters to formatC
+#' @param dtime_formatC a list of parameters to formatC
+#' @param num_fun a function to be applied to all numeric columns
+#' @param date_fun a function to be applied to all date columns
+#' @param dtime_fun a function to be applied to all POSIXt columns
+#' @param col_fun a function to be applied to all columns
 #'
 #' @return
 #' @export
 #'
 #' @examples
-df_format <- function(dat, num_format, date_format, format_fun) {UseMethod('df_format')}
+df_format <- function(dat, num_formatC, date_formatC, dtime_formatC,
+                      num_fun, date_fun, dtime_fun, col_fun){
+  UseMethod('df_format')
+}
 
-df_format.default <- function(dat, num_format = list(digits = 1, format = 'f'), date_format = NULL, format_fun = identity){
+df_format.default <- function(dat,
+                              num_formatC   = NULL,
+                              date_formatC  = NULL,
+                              dtime_formatC = NULL,
+                              num_fun       = identity,
+                              date_fun      = identity,
+                              dtime_fun     = identity,
+                              col_fun       = identity){
+
   dat <- as.data.frame(dat)
 
-  num_frmt      <- function(.x) do.call(formatC, args = c(list(x = .x),  num_format))
-  num_vars      <- names(dat)[unlist(lapply(dat, class)) == 'numeric']
-  dat[num_vars] <- lapply(dat[num_vars], num_frmt)
+  num_frmt  <- function(.x) do.call(formatC, args = c(list(x = .x),  num_formatC))
+  num_frmt  <- purrr::compose(num_frmt, num_fun)
 
-  if(!is.null(date_format)){
-    stop('not yet implemented')
+  params      <- list(
+    'numeric' = list(fc = num_formatC,   fun = num_fun),
+    'Date'    = list(fc = date_formatC,  fun = date_fun),
+    'POSIXt'  = list(fc = dtime_formatC, fun = dtime_fun)
+  )
+
+  res <- dat
+
+  for(i in c('numeric', 'Date', 'POSIXt')){
+    target_vars <- names(dat)[unlist(lapply(dat, is_any_class, i))]
+    fc          <- params[[i]]$fc
+    ff          <- params[[i]]$fun
+
+    ff %assert_class% 'function'
+
+    if(is.null(fc)){
+      formatter <- ff
+    } else {
+      fc %assert_class% 'list'
+      formatter <- function(.x) do.call(formatC, args = c(list(x = .x),  fc))
+      formatter <- purrr::compose(formatter, ff)
+    }
+
+  res[target_vars] <- lapply(res[target_vars],   formatter)
   }
 
-  dat           <- purrr::map_df(dat, format_fun)
+  res[]           <- lapply(res[], col_fun)
 
-  return(dat)
+  return(res)
 }
 
 
-df_format.data.table <- function(dat, num_format = list(digits = 1, format = 'f'), date_format = NULL, format_fun = identity){
-  dat <- data.table::copy(dat)
 
-  num_frmt      <- function(.x) do.call(formatC, args = c(list(x = .x),  num_format))
-  num_vars      <- names(dat)[unlist(lapply(dat, class)) == 'numeric']
-
-  dat[, (num_vars) := lapply(.SD, num_frmt), .SDcols = num_vars]
-
-  if(!is.null(date_format)){
-    stop('not yet implemented')
-  }
-
-  dat[,] <- lapply(dat[,], format_fun)
-
-  return(dat)
-}
