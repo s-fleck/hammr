bd06816bd9708eb10da24e8c7235e81a9fb80804
stefan <unabledispute@gmail.com>
2016-11-20 11:08:55 +0100

    more tests

--- tests/testthat/test_df_format.R
+++ tests/testthat/test_df_format.R
@@ -22,10 +22,48 @@ test_that("classes get preserved.", {
   rdt <- df_format(tdt)
   rti <- df_format(tti)
 
-  expect_identical(class(rdf), 'data.frame')
-  expect_identical(class(rdt), c('data.table', 'data.frame'))
+  expect_identical(rdf, tdf)
+  expect_identical(rdt, rdf)
+  expect_identical(rti, rdf)
 
-  df_format(tdf, num_formatC = list(digits = 1, format = 'f'))
+  parenthesise <- function(x) paste0('(', trimws(x) , ')')
 
+  tres <- df_format(tdf,
+                    num_format   = list(digits = 3, big.mark = '.', decimal.mark = ','),
+                    date_format  = list('%m/%d/%y'),
+                    dtime_format = list('%m/%d/%y %H:%M:%S'),
+                    col_format   = parenthesise)
+
+  tres2 <- df_format(tdf,
+                    num_format   = list(digits = 3, big.mark = '.', decimal.mark = ','),
+                    date_format  = '%m/%d/%y',
+                    dtime_format = '%m/%d/%y %H:%M:%S',
+                    col_format   = parenthesise)
+
+  eres <- data.frame(
+    a = c("(alpha)", "(beta)", "(ceta)"),
+    b = c("(1,23)",
+          "(-1.124,00)", "(1,90)"),
+    c = c("(1)", "(3)", "(5)"),
+    d = c("(01/12/09)",
+          "(01/12/09)", "(01/12/09)"),
+    t = c(
+      "(01/12/09 10:01:01)",
+      "(01/12/09 23:01:03)",
+      "(01/12/09 16:01:01)"
+    ), stringsAsFactors = FALSE
+  )
+
+  expect_identical(tres, eres)
+
+  expect_identical(
+    df_format(tdf, dtime_format = '%m/%d/%y %H:%M:%S'),
+    df_format(tdf, dtime_format = list(format = '%m/%d/%y %H:%M:%S'))
+  )
+
+  expect_identical(
+    df_format(tdf, num_format = 2),
+    df_format(tdf, num_format = list(digits = 2))
+  )
 
 })
