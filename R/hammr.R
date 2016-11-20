997e68375f7eb3c826bac41b20f53c91481e2523
stefan <unabledispute@gmail.com>
2016-11-20 11:09:57 +0100

    fixed imports

--- R/hammr.R
+++ R/hammr.R
@@ -3,7 +3,7 @@
 #' @section Foo functions:
 #' The foo functions ...
 #'
-#' @import assertthat magrittr dplyr data.table
+#' @import assertthat magrittr data.table dplyr dtplyr bit64 foreach openxlsx stringi xtable
 #' @docType package
 #' @name hammr
 NULL
