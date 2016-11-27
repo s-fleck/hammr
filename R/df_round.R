2c6e9167d64663c2e02cb55f7f30f254a0e7907d
stefan <unabledispute@gmail.com>
2016-11-27 14:34:53 +0100

    added df version of signif()

--- R/df_round.R
+++ R/df_round.R
@@ -16,3 +16,23 @@ df_round <- function(dat, digits = 0){
 
   return(dat)
 }
+
+
+#' Title
+#'
+#' @param dat
+#' @param digits
+#'
+#' @return
+#' @export
+#'
+#' @examples
+df_signif<- function(dat, digits = 0){
+  numcols <- names(dat)[lapply(dat, class) %in% c('numeric', 'integer')]
+
+  for(i in numcols){
+    dat[[i]] <- signif(dat[[i]], digits = digits)
+  }
+
+  return(dat)
+}
