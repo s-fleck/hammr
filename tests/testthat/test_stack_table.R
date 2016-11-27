2901e264aba9805012627edf5e6bb92e9ed11f8b
stefan <unabledispute@gmail.com>
2016-11-27 14:20:52 +0100

    prepared tests (but not implemented them yet)

--- tests/testthat/test_stack_table.R
+++ tests/testthat/test_stack_table.R
@@ -37,13 +37,42 @@ test_that('stacking tables by row works', {
     res1 <- stack_rows(st1)
     res2 <- stack_rows(st2)
 
+  # Create a col_stacked data.table
+    expect_error(stack_rows(tdat1))
+    res1 <- stack_cols(st1)
+    res2 <- stack_rows(st2)
+
+    stack_rows(st1)
+    stack_rows_tex(st1, insert_empty_row = TRUE)
+    stack_rows_tex(st1, insert_empty_row = FALSE)
+    print_tex(st1, stack_method = 'row')
+
+    stack_cols(st1)
+    stack_cols_tex(st1)
+    print_tex(st1, stack_method = 'col')
+
     expect_identical(lapply(res1, class), lapply(tdat1, class))
     expect_identical(as.character(lapply(res2, class)), as.character(lapply(tdat3, class)))
 
   # Create row stacked latex table
+    print_tex(st1)
+})
 
+test_that('stacking tables by col', {
+  # Creating stack tables
+  expect_error(stack_table(tdat1, tdat2))
+  expect_silent(st1 <- stack_table(tdat1, tdat2, rem_ext = '_xt'))
+  expect_silent(st2 <- stack_table(tdat1, tdat3, rem_ext = '_xt'))
 
+  # Create row stacked data.table
+  expect_error(stack_rows(tdat1))
+  res1 <- stack_rows(st1)
+  res2 <- stack_rows(st2)
 
+  expect_identical(lapply(res1, class), lapply(tdat1, class))
+  expect_identical(as.character(lapply(res2, class)), as.character(lapply(tdat3, class)))
 
+  # Create row stacked latex table
+  print_tex(st1)
 })
 
