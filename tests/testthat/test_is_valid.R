acff70399b4428e1dc6df68dc9bb3b06422c59ac
Stefan Fleck <stefan.fleck@statistik.gv.at>
2017-01-30 10:37:15 +0100

    removed uneccesary parenthesis

--- tests/testthat/test_is_valid.R
+++ tests/testthat/test_is_valid.R
@@ -10,7 +10,7 @@ test_that("is_valid works as expected", {
   is_valid.test <- function(testx) return(FALSE)
 
   tfun <- function(xtt){
-    assert_valid((xtt))
+    assert_valid(xtt)
   }
 
   expect_error(tfun(testx), 'A validity check failed for object of class: test, rest.')
