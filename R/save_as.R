20de485e2b6540e74d0d0dd52d0cbb7722472f02
Stefan Fleck <stefan.fleck@statistik.gv.at>
2017-02-07 15:59:25 +0100

    save_xlsx is now part of tatool

--- R/save_as.R
+++ R/save_as.R
@@ -15,31 +15,12 @@ save_as <- function(dat, outfile, format, ...){
 }
 
 
+
 #' @export
 save_txt <- function(dat, outfile, overwrite = TRUE){
   UseMethod('save_txt')
 }
 
-#' As workbook
-#'
-#' @param dat
-#' @param outfile
-#' @param overwrite
-#'
-#' @return
-#' @export
-#'
-#' @examples
-save_xlsx <- function(dat, outfile, overwrite){
-  UseMethod('save_xlsx')
-}
-
-
-#' @export
-save_xlsx.default <- function(dat, outfile, overwrite = FALSE){
-  wb <- as_workbook(dat)
-  openxlsx::saveWorkbook(wb, outfile, overwrite)
-}
 
 
 #' @export
