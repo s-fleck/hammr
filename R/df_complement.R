0e461e10c0e095b929c4e5908de1efdc5ac39528
stefan.b.fleck@gmail.com <stefan.fleck@statistik.gv.at>
2017-03-09 09:02:59 +0100

    refactoring, documentation

--- R/df_complement.R
+++ R/df_complement.R
@@ -1,36 +1,121 @@
-#' Title
+#' Complement a data.frame
 #'
-#' @param dat
-#' @param values
-#' @param col
-#' @param sort
+#' Adds empty rows to a \code{data.frame} based values in an id column or a
+#' combination of id columns. The intended use of this function is to ensure
+#' that \code{data.frames} that result from aggreating larger \code{data.frames}
+#' contain the same groups.
 #'
-#' @return
+#' @param dat a \code{data.frame}
+#' @param complement a \code{list} of the form \code{list(col1 = c("value1",
+#'   "value2", ...), col2 = c(1, 2, ...)), ...}. The names of \code{complement}
+#'   must correspond to column names of \code{dat}, and all list elements of
+#'   \code{complement} must have the same length.
+#' @return a \code{data.frame} containing all rows from \code{dat} and extra
+#'   rows for all values of \code{complement}
 #' @export
 #'
 #' @examples
-df_complement <- function(dat, values, col, sort = FALSE){
-  assert_that(is.scalar(col))
-  assert_that(is.vector(values) || is.factor(values))
+#'
+#' df1 <- data.frame(
+#'   g1 = c("a", "b", "c"),
+#'   g2 = c("A", "A", "C"),
+#'   da = 1:3
+#' )
+#'
+#' comp = list(
+#'   g1 = c("a", "d", "c"),
+#'   g2 = c("A", "B", "B")
+#' )
+#'
+#' df2 <- data.frame(
+#'   g1 = c("a", "d", "c"),
+#'   g2 = c("A", "B", "B"),
+#'   da = 7:9
+#' )
+#'
+#'
+#' df_complement(
+#'   df1,
+#'   list(
+#'     g1 = df2$g1,
+#'     g2 = df2$g2
+#'   )
+#' )
+#'
+#' #    g1 g2 da
+#' # 1:  a  A  1
+#' # 2:  b  A  2
+#' # 3:  c  C  3
+#' # 4:  d  B NA
+#' # 5:  c  B NA
+#'
+#' df_complement(
+#'   df2,
+#'   list(
+#'     g1 = df$g1,
+#'     g2 = df$g2
+#'   )
+#' )
+#'
+#' #    g1 g2 da
+#' # 1:  a  A  7
+#' # 2:  d  B  8
+#' # 3:  c  B  9
+#' # 4:  b  A NA
+#' # 5:  c  C NA
+#'
+df_complement <- function(
+  dat,
+  complement
+){
+  # Pre-conditions
+    assert_that(hammr::all_unique(names(dat), silent = TRUE))
+    assert_that(hammr::all_unique(names(complement), silent = TRUE))
 
-  dd <- data.table::copy(dat)
+    assert_that(is.list(complement))
+    assert_that(all(names(complement) %in% names(dat)))
 
-  if(is.factor(dat[[col]])){
-    dd[[col]] <- as.character(dd[[col]])
-  }
+    assert_that(suppressWarnings(hammr::all_identical(
+      lapply(complement, length))
+    ))
 
-  if(is.factor(values)){
-    values <- as.character(values)
-  }
+    assert_that(length(complement) > 0)
 
-  all_values <- list(c(dd[[col]], values[!values %in% dat[[col]]]))
-  names(all_values) <- col
-  all_values <- as.data.frame(all_values)
 
-  merge(
-    as.data.frame(dd),
-    as.data.frame(all_values),
-    by = col,
-    all = TRUE
-  )
+  # Setup
+    dd     <- as.data.table(data.table::copy(dat))
+
+    col    <- names(complement)
+    notcol <- names(dd)[!names(dd) %in% col]
+    newcol <- dd[1]
+    newcol[, (notcol) := NA]
+
+    extract_values <- function(comp, i){
+      as.character(comp[[i]])
+    }
+
+
+  # Logic
+    for(i_comp in seq_along(complement[[1]])){
+      values <- purrr::map(complement, extract_values, i_comp)
+
+      for(i_row in seq_len(nrow(dd))){
+        is_match <- identical(
+          values,
+          lapply(as.list(dd[i_row, (col), with = FALSE]), as.character)
+        )
+
+        if (is_match) {
+          break
+        } else if (identical(i_row, nrow(dd))) {
+          newcol[, (col) := values]
+          dd <- rbind(
+            dd,
+            newcol
+          )
+        }
+      }
+    }
+
+  return(dd)
 }
