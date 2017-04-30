15defe35d6c2cc806c5daaa1e0c07165c658a6c1
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-04-30 13:49:57 +0200

    cleaner interface to vec_match_sequence

--- R/vec_match_sequence.R
+++ R/vec_match_sequence.R
@@ -2,27 +2,64 @@
 #'
 #' \code{vec_match_seq} returns a vector of the positions of the matches of
 #' a sequence inside a longer vector. The index coresponds to the where the
-#' first element of the sequence matches the vector.
+#' first element of the sequence matches the vector. Please note that `NA`
+#' matches `NA` to be consistent with [match()]
 #'
 #' `%seq_in%` returns a logical vector of the matching described above
 #'
 #' @param sequence sequence to be matched
 #' @param target   target vector for sequence matching
 #'
+#' @return vec_match_seq returns the indicies of the first elements of the
+#'   matches between sequence and target. vec_match_seq_lgl returns a logical
+#'   vector with the same length as `sequence`
+#'
 #' @family vector tools
 #'
 #' @rdname vec_match_seq
 #' @export
+#'
+#' @examples
+#'
+#' t <- c(1, 2, 3, 4, 3, 2, 1)
+#' s <- c(3, 4)
+#'
+#' vec_match_seq(s, t)
+#' vec_match_seq_lgl(s, t)
+#' s %seq_in% t
+#'
 vec_match_seq <- function(sequence, target){
-  which(sequence %seq_in% target)
+  which(vec_match_seq_internal(sequence, target))
 }
+
+
+
+
+#' @rdname vec_match_seq
+vec_match_seq_lgl <- function(sequence, target){
+  res  <- vec_match_seq_internal(sequence, target)
+  to_fill <- seq.int((length(res) + 1L), length(target))
+  res[to_fill] <- FALSE
+  res
+}
+
+
 
 
 #' @rdname vec_match_seq
 #' @export
 `%seq_in%` <- function(sequence, target){
+  any(vec_match_seq_internal(sequence, target))
+}
+
+
+
+
+# utils -------------------------------------------------------------------
+
+vec_match_seq_internal <- function(sequence, target){
   group_matches <- function(x, matches, sequence, target){
-    sub <- x:(length(sequence) + x - 1)
+    sub <- seq.int(x, (length(sequence) + x - 1L))
     matches[sub]
   }
 
@@ -31,15 +68,11 @@ vec_match_seq <- function(sequence, target){
     all(purrr::map_lgl(seq_along(tmp), function(x) tmp[[x]][[x]]))
   }
 
-
   matches    <- purrr::map(target, equal_or_na, sequence)
-  iterations <- 1:(length(target) - length(sequence) + 1)
-  matches    <- purrr::map(iterations, group_matches,
-                           matches, sequence, target)
-
+  iterations <- seq_len(length(target) - length(sequence) + 1)
+  matches    <- purrr::map(
+    iterations, group_matches, matches, sequence, target
+  )
   res <- purrr::at_depth(matches, 1, add_elements)
-
-  fill <- (length(res) + 1):length(target)
-  res[fill] <- FALSE
   unlist(res)
 }
--- tests/testthat/test_vec_match_sequence.R
+++ tests/testthat/test_vec_match_sequence.R
@@ -8,8 +8,8 @@ s2 <- c('m', 'a', 't', 'c', 'h')
 
 
 test_that("vec_match_seq: matching sequence to vector works.", {
-  expect_identical(length(s1 %seq_in% t1), length(t1))
-  expect_identical(length(s2 %seq_in% t2), length(t2))
+  expect_identical(length(vec_match_seq_lgl(s1, t1)), length(t1))
+  expect_identical(length(vec_match_seq_lgl(s2, t2)), length(t2))
 
   expect_identical(vec_match_seq(s1, t1), c(1L, 21L, 31L))
   expect_identical(vec_match_seq(s2, t2), 4L)
