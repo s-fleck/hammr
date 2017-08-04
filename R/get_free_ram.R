be3a126623ef1df2eaf91a22b71cc8ff4282d65f
stefan.b.fleck@gmail.com <stefan.fleck@statistik.gv.at>
2017-08-04 10:29:37 +0200

    Added unit parameter to get_free_ram

--- R/get_free_ram.R
+++ R/get_free_ram.R
@@ -1,15 +1,28 @@
 #' Get free phyiscal memory
 #'
+#' @unit any of `b`, `kb`, `kib`, `mb`, `mib`, `gb`, `gib`
 #' @return Available system memory in bytes
 #' @export
 #'
-get_free_ram <- function(){
+get_free_ram <- function(unit = "kib"){
+
+  unit <- tolower(unit)
+
   if(Sys.info()[["sysname"]] == "Windows"){
     x <- system2("wmic", args =  "OS get FreePhysicalMemory /Value", stdout = TRUE)
     x <- x[grepl("FreePhysicalMemory", x)]
     x <- gsub("FreePhysicalMemory=", "", x, fixed = TRUE)
     x <- gsub("\r", "", x, fixed = TRUE)
-    as.integer(x)
+    res <- as.integer(x)
+
+    switch(unit,
+      "kib" = res,
+      "mib" = res / 1024,
+      "gib" = res / 1024^2,
+      stop("Unit not valid")
+    )
+
+
   } else {
     stop("Only supported on Windows OS")
   }
