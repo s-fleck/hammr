5f4026b1b07579a31144ee93fe3b1a1e8ef2f2a6
stefan.b.fleck@gmail.com <stefan.fleck@statistik.gv.at>
2017-08-24 10:33:03 +0200

    * added wday2: ISO compliant wrapper for lubridate::wday

---
+++ R/wday2.R
@@ -0,0 +1,25 @@
+#' Get Weekday Component of a date-time (ISO 8601 Compliant)
+#'
+#' A wrapper for [lubridate::wday()] that returns the weekdays in `ISO 8601`
+#' order (Monday is first), while [lubridate] uses the freedom order (Sunday
+#' is first).
+#'
+#' @inheritParams lubridate::wday
+#'
+#' @return wday2 returns the day of the week as a decimal number
+#'   (01-07, Monday is 1) or an ordered factor (Monday is first).
+#' @export
+#'
+wday2 <- function(x, label = FALSE, abbr = TRUE){
+  res <- lubridate::wday(x = x, label = label, abbr = abbr)
+
+  if(label){
+    sun_name <- grep("sun.*", levels(res), value = TRUE, ignore.case = TRUE)
+    res <- forcats::fct_relevel(res, sun_name, after = length(res))
+  } else {
+    res <- as.integer(res) - 1L
+    res[res == 0] <- 7L
+  }
+
+  res
+}
