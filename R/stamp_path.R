fc7b7a41b5f6c5011e3ad2d6faafb8c0f3403f77
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-01-10 12:19:35 +0100

    doc stamp path and added preset tokens

--- R/stamp_path.R
+++ R/stamp_path.R
@@ -1,17 +1,31 @@
-#' Title
+#' Add a Stamp to a Path
 #'
-#' @param x
-#' @param stamp
+#' @param x a `character` vector
+#' @param stamp an atomic scalar. Before it is added to the output path between
+#'   the filename and the file extension it is passed through [base::format()].
+#'   The special strings `":date:"`, `":time:`, `":timestamp:"` can be used for
+#'   the current date/time/datetime in ISO format without separators.
+#' @param ... passed on to  `base::format()`
 #'
-#' @return
+#' @return a `character` vector of the same length as `x`
 #' @export
 #'
-#' @examples
 stamp_path <- function(
   x,
-  stamp = as.character(Sys.Date())
+  stamp = format(Sys.time(), "%Y%m%dT%H%M%S"),
+  ...
 ){
-  stopifnot(is_scalar_character(stamp))
+  switch(
+    stamp,
+    ":date:" = format(Sys.date(), "%Y%m%d"),
+    ":time:" = format(Sys.time(), "H%M%S"),
+    ":timestamp:" = format(Sys.time(), "%Y%m%dT%H%M%S")
+  )
+
+
+  stopifnot(is_scalar_atomic(stamp))
+  stopifnot(is.character(x))
+  stamp <- format(stamp, ...)
   bp  <- tools::file_path_sans_ext(x)
   ext <- substr(x, nchar(bp) + 1, nchar(x))
 
