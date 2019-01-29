aa44c2dcd0b936f451389b2734a8e34f56747b97
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-01-29 14:18:09 +0100

    documented stamp_path, and stamp_path now behaves as intended for custom stamps

--- R/stamp_path.R
+++ R/stamp_path.R
@@ -5,10 +5,22 @@
 #'   the filename and the file extension it is passed through [base::format()].
 #'   The special strings `":date:"`, `":time:"`, `":timestamp:"` can be used for
 #'   the current date/time/datetime in ISO format without separators.
-#' @param ... passed on to  `base::format()`
+#' @param stamp_fmt `character` scalar. Assumes that the `format()` method for
+#'   whatever class `stamp` is has a `format` argument (otherwise it is silently
+#'   ignored). This is for example true for the `Date`, `POSIXt`, and
+#'   [dint][dint::dint-package] dates, for which sensible default formats
+#'   are defined.
+#' @param ... passed on to  [format()]
 #'
 #' @return a `character` vector of the same length as `x`
 #' @export
+#'
+#' path <- "path/to/important.xlsx"
+#' stamp_path(path)
+#' stamp_path(path, ":date:")
+#' stamp_path(path, ":time:")
+#' stamp_path(path, ":timestamp:")
+#' stamp_path(path, "hello")
 #'
 stamp_path <- function(
   x,
@@ -23,11 +35,12 @@ stamp_path <- function(
   },
   ...
 ){
-  switch(
+  stamp <- switch(
     stamp,
     ":date:" = format(Sys.Date(), "%Y%m%d"),
-    ":time:" = format(Sys.time(), "H%M%S"),
-    ":timestamp:" = format(Sys.time(), "%Y%m%dT%H%M%S")
+    ":time:" = format(Sys.time(), "%H%M%S"),
+    ":timestamp:" = format(Sys.time(), "%Y%m%dT%H%M%S"),
+    stamp
   )
 
 
