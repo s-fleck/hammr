b689f52191b5552b097852364be4e01edc1c4719
Stefan Fleck <stefan.fleck@statistik.gv.at>
2020-02-13 08:16:45 +0100

    added save_multipage_pdf

---
+++ R/save_multipage_pdf.R
@@ -0,0 +1,44 @@
+#' Save a list of plots
+#'
+#' @param x a `list` of [ggplot2::ggplot()] objects or anything supported by
+#'   [grid::grid.draw]
+#' @param filename `scalar` character, a file system path. Filename should end
+#'   in `'.pdf'`.
+#' @param width,height `numeric` scalars, Dimension (in `inch`)
+#' @param
+#'
+#' @note
+#'
+#' \describe{
+#'   \item{A4}{`width =  8.27, height = 11.69`}
+#'   \item{A3}{`width = 11.69, height = 16.53`}
+#' }
+#'
+#' @return `filename` (invisibly)
+#' @export
+save_multipage_pdf = function(
+  x,
+  filename,
+  width,
+  height,
+  overwrite = FALSE
+){
+  if (file.exists(filename)){
+    if (overwrite)
+      unlink(filename)
+    else
+      stop("File '", filename, "' exists and overwrite == FALSE")
+  }
+
+  pdf(filename, onefile = TRUE, width = width, height = height)
+  on.exit(dev.off())
+
+  for (i in seq_along(x)){
+    grid::grid.draw(p[[i]])
+    if (i < length(x)){
+      grid::grid.newpage()
+    }
+  }
+
+  invisible(filename)
+}
